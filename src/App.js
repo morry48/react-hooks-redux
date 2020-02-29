a36c9db14ba58b27f40a26cbb8f2e93b161717e2
morry48 <yyyyuuuuuuuu0408@gmail.com>
2020-02-29 20:36:59 +0900

    my frist react Hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increament = () => setCount(count + 1);
+  const decreament = () => setCount(count - 1);
+
+  const increament2 = () => setCount(previounsCount => previounsCount + 1);
+  const decreament2 = () => setCount(previounsCount => previounsCount + 2);
+
+  const reset =() => setCount(0);
+  const double = () =>  setCount(count * 2);
+  const devide3 = () => {
+    if(count % 3 == 0){
+      return setCount(count / 3);
+    }
+  }
+
+
   return (
-    <div>
-      this is a pen
-    </div>
+    <React.Fragment>
+      <div>count: {count} </div>
+      <button onClick={increament}>+1</button>
+      <button onClick={decreament}>-1</button>
+      <div>
+        <button onClick={increament2}>+1</button>
+        <button onClick={decreament2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>double</button>
+        <button onClick={devide3}>３の倍数のときだけ３で割る</button>
+      </div>
+    </React.Fragment>
   );
 }
 
