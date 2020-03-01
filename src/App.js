6e6a0f8cb675ee79a6e5d70aa891087f0884fd60
morry48 <yyyyuuuuuuuu0408@gmail.com>
2020-03-01 23:36:03 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import React, { useState } from 'react';
 
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = props => {
 
-  const increament = () => setCount(count + 1);
-  const decreament = () => setCount(count - 1);
-
-  const increament2 = () => setCount(previounsCount => previounsCount + 1);
-  const decreament2 = () => setCount(previounsCount => previounsCount + 2);
-
-  const reset =() => setCount(0);
-  const double = () =>  setCount(count * 2);
-  const devide3 = () => {
-    if(count % 3 == 0){
-      return setCount(count / 3);
-    }
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
   }
-
-
   return (
-    <React.Fragment>
-      <div>count: {count} </div>
-      <button onClick={increament}>+1</button>
-      <button onClick={decreament}>-1</button>
-      <div>
-        <button onClick={increament2}>+1</button>
-        <button onClick={decreament2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>double</button>
-        <button onClick={devide3}>３の倍数のときだけ３で割る</button>
-      </div>
-    </React.Fragment>
+    <>
+      <p>現在の{name}は、{price}円です</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input  value={name}　onChange={e => setName(e.target.value)}/>
+    </>
   );
+}
+
+App.defaultProps = {
+  name:'サンプル',
+  price:1000
 }
 
 export default App;
