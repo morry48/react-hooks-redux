74b0271a7e7e89e2ad694d22964f7f70a4d72654
morry48 <yyyyuuuuuuuu0408@gmail.com>
2020-03-03 00:51:53 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 const App = props => {
 
   const [state, setState] = useState(props)
   const { name, price } = state 
+
+  useEffect(() => {
+    console.log('This is like componentDidmount or componentDidupdate.')
+  });
+
+  useEffect(() => {
+    console.log('this is componentDidmount');
+  },[]);
+
+  useEffect(() => {
+    console.log('this callback is for name only');
+  },[name]);
+
   return (
     <>
       <p>現在の{name}は、{price}円です</p>
