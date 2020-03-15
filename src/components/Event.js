a2c451593e95e6e02893bff2d8b6c4167a9e1735
morry48 <yyyyuuuuuuuu0408@gmail.com>
2020-03-15 13:46:56 +0900

    delete operation log

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,13 +1,23 @@
 import React, { useContext } from 'react';
-import { DELETE_EVENT } from '../actions';
+import { DELETE_EVENT, ADD_OPERATION_LOG } from '../actions';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const Event = ({ event }) => {
     const { dispatch } = useContext(AppContext);
     const id = event.id
     const  handleClickDeleteButton = () => {
         const result = window.confirm(`イベント(id=${id})を本当に削除してもいいですか？`)
-        if (result) {dispatch({ type: DELETE_EVENT, id })}
+        if (result) {
+          dispatch({ type: DELETE_EVENT, id })
+
+          dispatch({
+            type: ADD_OPERATION_LOG,
+            description: `イベントを（id=${id}を削除しました。）`,
+            operatedAt: timeCurrentIso8601()
+          })
+        }
+
     }
 
     return(
